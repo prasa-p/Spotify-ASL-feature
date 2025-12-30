f44ee96a03199b829f2f37725e2cd0492e12a9c8
Prasa <prasa.pirabagaran@gmail.com>
2025-12-30 09:54:57 -0500

    Initial Commit

---
+++ NEW Spotify ASL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NEW Spotify ASL
+//
+//  Created by Prasa Pirabagaran on 2025-12-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NEW Spotify ASL/NEW_Spotify_ASLApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEW_Spotify_ASLApp.swift
+//  NEW Spotify ASL
+//
+//  Created by Prasa Pirabagaran on 2025-12-30.
+//
+
+import SwiftUI
+
+@main
+struct NEW_Spotify_ASLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
